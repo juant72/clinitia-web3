aacd922cd6cbaa9dc448de5e065b37e2fde0ddba
Juan Tapia <juant72@gmail.com>
2024-08-13 11:46:06 -0300

    Success finisihed

--- client/app/patients/[userId]/new-appointment/success/page.tsx
+++ client/app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { getAppointment } from "@/lib/actions/appointment.action";
+import { Doctors } from "@/constants";
+import { formatDateTime } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-const Succss = () => {
+const Success = async ({ params: {userId}, searchParams}:SearchParamProps) => {
+    const appointmentId=(searchParams?.appointmentId as string) || '';
+    const appointment= await getAppointment(appointmentId);
+    const doctor =Doctors.find((doc) => doc.name==appointment.primaryPhysician);
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
@@ -31,20 +39,36 @@ const Succss = () => {
         </section>
 
         <section className="request-details">
-            <p>Requested appointment details:</p>
-            <div className="flex items-center gap-3">
-                <Image 
-                    src=""
-                    height={}
-                    width={}
-                    alt=""
-                />
-            </div>
-
+          <p>Requested appointment details:</p>
+          <div className="flex items-center gap-3">
+            <Image 
+                src={doctor?.image!} 
+                height={100} 
+                width={100} 
+                alt="doctor"
+                className="size-6" 
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+                src="/assets/icons/calendar.svg"
+                height={24}
+                width={24}
+                alt="calendar"
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
+        <Button variant="outline" className="shad-primary-btn" asChild>
+            <Link href={`/patients/${userId}/new-appointment`}>
+                New Appointment
+            </Link>            
+        </Button>
+        <p className="copyright">© 2024 Clinitia</p>
       </div>
     </div>
   );
 };
 
-export default Succss;
+export default Success;
