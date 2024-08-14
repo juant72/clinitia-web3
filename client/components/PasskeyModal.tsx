1b0165bc69d23fdf3041a6435626f2d78d5243dc
Juan Tapia <juant72@gmail.com>
2024-08-14 14:41:41 -0300

    Passkey first version

--- client/components/PasskeyModal.tsx
+++ client/components/PasskeyModal.tsx
@@ -1,4 +1,11 @@
-'use client'
+"use client";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 import {
   AlertDialog,
@@ -12,11 +19,49 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
-import React, { useState } from "react";
-import Image from 'next/image';
+import React, { useEffect, useReducer, useState } from "react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 const PasskeyModal = () => {
-    const [open, setOpen] = useState(true);
+  const router = useRouter();
+  const path=usePathname();
+
+  const [passkey  , setPasskey] = useState('');
+  const [error, setError] = useState('');
+  const [open, setOpen] = useState(true);
+  const encryptedKey=typeof window !=='undefined' ? window.localStorage.getItem('accessKey'):null;
+
+  useEffect(() => {
+    const accessKey=encryptedKey && decryptKey(encryptedKey);
+
+    if (path){
+      if (accessKey===process.env.NEXT_PUBLIC_ADMIN_PASSKEY){
+        setOpen(false);
+        router.push('/admin');
+      } else{
+        setOpen(true);
+      }
+    }
+  }, [encryptedKey]);
+  
+  
+  const validatePasskey= (e: React.MouseEvent<HTMLButtonElement,MouseEvent>)=>{
+    e.preventDefault();
+    if (passkey===process.env.NEXT_PUBLIC_ADMIN_PASSKEY){
+      const encryptedKey=encryptKey(passkey);
+      localStorage.setItem('accessKey',encryptedKey);
+      setOpen(false);
+    } else{
+      setError('Invalid passkey. Please try again.');
+    }
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
@@ -24,21 +69,40 @@ const PasskeyModal = () => {
         <AlertDialogHeader>
           <AlertDialogTitle className="flex items-start justify-between">
             Admin Access Verification
-            <Image 
-                src="/assets/icons/close.svg"
-                alt="close"
-                width={20}
-                height={20}
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
             />
-            </AlertDialogTitle>
+          </AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            To access the admin page, please enter the passkey.
           </AlertDialogDescription>
         </AlertDialogHeader>
+        <div>
+          <InputOTP maxLength={6} value={passkey} onChange={(value)=>setPasskey(value)}>
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+
+          {error && <p className="shad-error text-14-regular mt-4 flex justify-center">
+            {error}
+            </p>}
+        </div>
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={(e)=>validatePasskey(e)}
+            className="shad-primary-btn w-full"
+            >
+            Enter Admin Passkey</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
